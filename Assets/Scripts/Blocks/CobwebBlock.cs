c0506d254a8201ea0a15fc73b0114006bcede15f
KirillGorb <kirillgor12506@gmail.com>
2024-02-25 11:59:00 +0300

    таймер замедления

--- Assets/Scripts/Blocks/CobwebBlock.cs
+++ Assets/Scripts/Blocks/CobwebBlock.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Play.Movement.Abstraction;
 using UnityEngine;
 
@@ -6,21 +7,33 @@ namespace Play.Block
     public class CobwebBlock : MonoBehaviour
     {
         [SerializeField] private float speedDown;
+        [SerializeField] private float timeSlow;
+
+        [SerializeField] private bool isSlowTime;
+
+        private ISettingMoveble _setting;
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.TryGetComponent(out ISettingMoveble setting))
             {
-                setting.MoveSetting.ModValueSpeed.AddModifSpeed(speedDown);
+                _setting = setting;
+                _setting.MoveSetting.ModValueSpeed.AddModifSpeed(speedDown);
+                if (isSlowTime)
+                    StartCoroutine(TimerSlowdowm());
             }
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if (other.TryGetComponent(out ISettingMoveble setting))
-            {
+            if (other.TryGetComponent(out ISettingMoveble setting) && !isSlowTime)
                 setting.MoveSetting.ModValueSpeed.RemoveModifSpeed(speedDown);
-            }
+        }
+
+        private IEnumerator TimerSlowdowm()
+        {
+            yield return new WaitForSeconds(timeSlow);
+            _setting.MoveSetting.ModValueSpeed.RemoveModifSpeed(speedDown);
         }
     }
 }
